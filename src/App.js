bcbaf3dee93509b90e6e23681ddcd1b1bb7e8c97
Camilo <camiloolave22@gmail.com>
2025-11-06 22:34:51 -0500

    implementacion frontend para funcionalidad cambiar contraseña

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ChangePassword from "./pages/ChangePassword";
+import Menu from "./pages/Menu";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
         <Route path="/change-password" element={<ChangePassword />} />
       </Routes>
     </Router>
